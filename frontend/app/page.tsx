0966b7bea3e208e921973c8285950f2c9840d28a
Favio V. F. <favio102@protonmail.com>
2024-07-26 06:11:17 -0700

    styles added, tailwind.congi update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <main className="overflow-hidden">test</main>;
+  return <main className="overflow-hidden">
+    <Hero/>
+  </main>;
 }
