31a9d4592e521bf363a3ca455dd2866c35241ac7
Favio V. F. <favio102@protonmail.com>
2024-07-27 01:03:49 -0700

    edit and remove btn added

--- frontend/components/BookCard.tsx
+++ frontend/components/BookCard.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 import BookDetails from "./BookDetails";
+import Link from "next/link";
 
 const BookCard = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,21 +26,40 @@ const BookCard = () => {
         <p className="flex mt-1 text-[12px] font-extrabold">Year</p>
       </div>
 
-      <div className="relative flex w-full mt-2">
-        <div className="book-card__btn-container">
+      <div className="relative flex flex-col w-full mt-2 group">
+        <Link href={`/books/id`} className="book-card__btn-container">
           <CustomButton
-            title="View More...."
+            title="View Details..."
             containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </Link>
+        <div className="book-card__edit-btn-container">
+          <CustomButton
+            title="Edit"
+            containerStyles="w-full py-[16px] rounded-full bg-amber-500"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+        <div className="book-card__delete-btn-container">
+          <CustomButton
+            title="Delete"
+            containerStyles="w-full py-[16px] rounded-full bg-red-700"
             textStyles="text-white text-[14px] leading-[17px] font-bold"
             rightIcon="/right-arrow.svg"
             handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
-      <BookDetails
+
+      {/* <BookDetails
         isOpen={isOpen}
         closeModal={() => setIsOpen(false)}
-      />
+      /> */}
     </div>
   );
 };
