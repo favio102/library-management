fe527afd567d9942c1ef4d6a8bf37d720943d42c
Favio V. F. <favio102@protonmail.com>
2024-07-28 21:56:42 -0700

    add btn update

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,14 +2,50 @@
 
 import Image from "next/image";
 import Hero from "@/components/Hero";
-import { BookCard, CustomButton, SearchBar } from "@/components";
+import { BookCard, BookDetails, CustomButton, SearchBar } from "@/components";
 import { useBooks } from "@/context/BookContext";
 import { useEffect, useState } from "react";
+import { BookProps } from "@/types";
 
 export default function Home() {
-  const { books, fetchBooks } = useBooks();
+  const { books, fetchBooks, addBook, updateBook } = useBooks();
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [bookId, setBookId] = useState<string | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [book, setBook] = useState<BookProps>({
+    id: "",
+    title: "",
+    author: "",
+    description: "",
+    year: "",
+    edition: "",
+    language: "",
+    subject: "",
+    format: "",
+    publisher: "",
+  });
+
+  const handleOpenModal = (editing: boolean, id: string | null = null) => {
+    setIsEditing(editing);
+    setBookId(id);
+    if (!editing) {
+      setBook({
+        id: "",
+        title: "",
+        author: "",
+        description: "",
+        year: "",
+        edition: "",
+        language: "",
+        subject: "",
+        format: "",
+        publisher: "",
+      });
+    }
+    setIsOpen(true);
+  };
 
   useEffect(() => {
     const loadBooks = async () => {
@@ -58,6 +94,19 @@ export default function Home() {
             containerStyles="text-fuchsia-400 rounded bg-white hover:bg-blue-100 min-w-[130px] me-6 border dark:border-slate-300"
           />
         </div>
+        <BookDetails
+          isOpen={isOpen}
+          closeModal={() => {
+            setIsOpen(false);
+            fetchBooks();
+          }}
+          isEditing={isEditing}
+          bookId={bookId}
+          book={book}
+          setBook={setBook}
+          onAddBook={addBook}
+          onUpdateBook={updateBook}
+        />
 
         {isLoading ? (
           <p>Loading...</p>
