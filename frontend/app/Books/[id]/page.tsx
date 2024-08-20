2d9cae946659d0336701c3157c5a1b9afdbb5b27
Favio V. F. <favio102@protonmail.com>
2024-08-19 23:13:42 -0700

    book page refactor

--- frontend/app/Books/[id]/page.tsx
+++ frontend/app/Books/[id]/page.tsx
@@ -72,7 +72,7 @@ const BookPage = () => {
   }
 
   return (
-    <section className="relative max-w-[1440px] mx-auto pt-16 md:pt-4 p-1">
+    <section className="relative max-w-[1440px] mx-6 pt-16 md:pt-4 p-1">
       <div className="flex flex-col md:flex-row gap-2 md:gap-6 w-full">
         <div className="bg-gray-100 w-full md:w-1/4 rounded-2xl flex justify-center items-center">
           <Image
@@ -84,54 +84,59 @@ const BookPage = () => {
           />
         </div>
         <div className="flex flex-col w-full md:w-3/4 p-4">
-          <h2 className="font-bold text-lg md:text-3xl">Title: {book.title}</h2>
-          <h5 className="font-bold text-lg md:text-3xl">
-            Author: {book.author}
-          </h5>
-          <p className="mt-1.5 text-gray-800 dark:text-gray-400 text-normal md:text-lg">
-            Description: {book.description}
-          </p>
+          <h4 className="md:text-lg capitalize text-gray-800 dark:text-gray-400">
+            Title
+          </h4>
+          <h2 className="font-bold text-lg md:text-3xl">{book.title}</h2>
+          <h4 className="md:text-lg capitalize text-gray-800 dark:text-gray-400">
+            Author
+          </h4>
+          <h5 className="font-semibold text-lg md:text-3xl">{book.author}</h5>
+          <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize">
+            Description
+          </h4>
+          <p className="mt-1.5 font-semibold md:text-lg">{book.description}</p>
           <div className="space-y-2 md:space-y-4 mt-4 md:mt-6 w-full">
             <div className="flex items-center justify-between w-full">
               <div className="w-1/2">
-                <h4 className="md:text-lg font-semibold capitalize">
+                <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize">
                   Publication Year
                 </h4>
-                <p className="font-normal">{book.year}</p>
+                <p className="font-semibold">{book.year}</p>
               </div>
               <div className="w-1/2">
-                <h4 className="md:text-lg font-semibold capitalize text-left">
+                <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize text-left">
                   Edition
                 </h4>
-                <p className="font-normal">{book.edition}</p>
+                <p className="font-semibold">{book.edition}</p>
               </div>
             </div>
             <div className="flex items-center justify-between w-full">
               <div className="w-1/2">
-                <h4 className="md:text-lg font-semibold capitalize">
+                <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize">
                   Language
                 </h4>
-                <p className="font-normal">{book.language}</p>
+                <p className="font-semibold">{book.language}</p>
               </div>
               <div className="w-1/2">
-                <h4 className="md:text-lg font-semibold capitalize">
+                <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize">
                   Subjects/Keywords
                 </h4>
-                <p className="font-normal">{book.subject}</p>
+                <p className="font-semibold">{book.subject}</p>
               </div>
             </div>
             <div className="flex items-center justify-between w-full">
               <div className="w-1/2">
-                <h4 className="md:text-lg font-semibold capitalize">
+                <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize">
                   Formats Available
                 </h4>
-                <p className="font-normal">{book.format}</p>
+                <p className="font-semibold">{book.format}</p>
               </div>
               <div className="w-1/2">
-                <h4 className="md:text-lg font-semibold capitalize">
+                <h4 className="md:text-lg text-gray-800 dark:text-gray-400 capitalize">
                   Publisher
                 </h4>
-                <p className="font-normal">{book.publisher}</p>
+                <p className="font-semibold">{book.publisher}</p>
               </div>
             </div>
           </div>
